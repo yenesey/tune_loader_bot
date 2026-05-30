b4edbf624d82d1db2d825a2a1ba3e39cb467e818
yenesey <denesey@gmail.com>
2026-05-30 16:28:17 +0300

    clean code

--- bot.py
+++ bot.py
@@ -1,46 +1,49 @@
 #!/usr/bin/python3
-import logging
 import asyncio
-import traceback
+import logging
 import os
-import urllib.parse
 import re
-import json
+import traceback
+import urllib.parse
 from datetime import datetime, timedelta
+from typing import Any, Awaitable, Callable, Dict
 
-from typing import Any, Callable, Dict, Awaitable
-
-from aiogram.filters import Command, CommandStart, StateFilter
-from aiogram.fsm.state import State, StatesGroup
-from aiogram.fsm.context import FSMContext
-from aiogram.utils.keyboard import InlineKeyboardBuilder
-from aiogram import Bot, Dispatcher #, Router
+from aiogram import Bot, Dispatcher  # , Router
+from aiogram.client.default import DefaultBotProperties
 from aiogram.dispatcher.middlewares.base import BaseMiddleware
+from aiogram.filters import StateFilter
+from aiogram.fsm.context import FSMContext
+from aiogram.fsm.state import State, StatesGroup
 from aiogram.types import (
-    TelegramObject,
-    Message,
+    CallbackQuery,
     FSInputFile,
+    InlineKeyboardButton,
     InputMediaAudio,
     InputMediaVideo,
-    BotCommand,
-    CallbackQuery,
-    ReplyKeyboardMarkup,
-    InlineKeyboardButton,
+    Message,
+    TelegramObject,
 )
+from aiogram.utils.keyboard import InlineKeyboardBuilder
 from aiogram.utils.markdown import hlink
-from aiogram.client.default import DefaultBotProperties
+
+from database import Database
+
 ######################################################################
 from download import download_media, download_playlist
+
 # from watchdog import watchdog
 from settings import SETTINGS
-from database import Database
+
 ######################################################################
 USER_DATA = {}
-DB = None
-TG_MAX_FILE_SIZE = 50*1024*1024
+TG_MAX_FILE_SIZE = 50 * 1024 * 1024
+DB = Database()
+
+
 ######################################################################
 class DownloadActions(StatesGroup):
     select_action = State()
+
 
 class WatchdogActions(StatesGroup):
     select_action = State()
@@ -52,38 +55,52 @@ class SecurityMiddleware(BaseMiddleware):
         self,
         handler: Callable[[TelegramObject, Dict[str, Any]], Awaitable[Any]],
         event: TelegramObject,
-        data: Dict[str, Any]
+        data: Dict[str, Any],
     ) -> Any:
         if "event_from_user" in data:
             user = data["event_from_user"]
-            if (user.id not in SETTINGS["users-list"]):
+            if user.id not in SETTINGS["users-list"]:
                 logging.info(f"Unknown user: {str(user.id)}")
                 logging.info(f"message.text: {data.keys()}")
-                return   
+                return
         return await handler(event, data)
+
 
 ##############################################################
 def sub_dir(on_date: datetime):
-    return os.path.join(str(on_date.year), f"{str(on_date.year)}-{str(on_date.month).zfill(2)}")
+    return os.path.join(
+        str(on_date.year), f"{str(on_date.year)}-{str(on_date.month).zfill(2)}"
+    )
+
 
 def enrich_info(info):
     if not info or not isinstance(info, dict):
         return None
     info["performer"] = info["artist"] or info["channel"]
-    info["link_text"] = info["performer"] + " - " + info["title"] if info["performer"] else info["title"]
-    info["server_url"] = f'{SETTINGS["server-root-url"]}/{sub_dir(info["date"])}/{urllib.parse.quote(info["file_name"])}'
-    if not 'file_path' in info:
-        info['file_path'] = os.path.join(f'{SETTINGS["download-dir"]}/{sub_dir(info["date"])}', info['file_name'])
+    info["link_text"] = (
+        info["performer"] + " - " + info["title"]
+        if info["performer"]
+        else info["title"]
+    )
+    info["server_url"] = (
+        f"{SETTINGS['server-root-url']}/{sub_dir(info['date'])}/{urllib.parse.quote(info['file_name'])}"
+    )
+    if "file_path" not in info:
+        info["file_path"] = os.path.join(
+            f"{SETTINGS['download-dir']}/{sub_dir(info['date'])}", info["file_name"]
+        )
     return info
 
-async def find(url: str, video: bool) -> dict:
+
+async def find(url: str, video: bool) -> dict | None:
     found = await DB.find_url(url, video)
     if found:
         return enrich_info(found)
 
-async def download(url: str, video: bool, user_id: str) -> dict:
+
+async def download(url: str, video: bool, user_id: str | None) -> dict:
     date = datetime.now()
-    target_dir = f'{SETTINGS["download-dir"]}/{sub_dir(date)}'
+    target_dir = f"{SETTINGS['download-dir']}/{sub_dir(date)}"
     if not os.path.isdir(target_dir):
         os.makedirs(target_dir)
     loaded = await download_media(target_dir, url, video)
@@ -91,14 +108,17 @@ async def download(url: str, video: bool, user_id: str) -> dict:
         loaded["date"] = date
         loaded["user_id"] = user_id
         loaded["url"] = url
-        loaded["video"] = int(video) 
+        loaded["video"] = int(video)
         loaded["delivered"] = int(False)
         await DB.save_download(loaded)
         enrich_info(loaded)
     return loaded
 
+
 async def process_message(message: Message, video: bool):
-    url = message.text
+    if not message.text:
+        return
+    url = message.text or ""
     InputMedia = InputMediaVideo if video else InputMediaAudio
     try:
         instant_answer = await message.answer("Processing. Please wait for a while...")
@@ -109,18 +129,26 @@ async def process_message(message: Message, video: bool):
         if result:
             if result["file_size"] < TG_MAX_FILE_SIZE:
                 await instant_answer.edit_media(
-                    InputMedia(media = FSInputFile(result["file_path"]),
-                        title = result["title"],
-                        performer = result["performer"],
-                        caption = hlink("#origin", url) + "  " + hlink("#file", result["server_url"]))
+                    InputMedia(
+                        media=FSInputFile(result["file_path"]),
+                        title=result["title"],
+                        performer=result["performer"],
+                        caption=hlink("#origin", url)
+                        + "  "
+                        + hlink("#file", result["server_url"]),
+                    )
                 )
             else:
-                await instant_answer.edit_text(hlink(result["link_text"], result["server_url"]) + "\n" + hlink("#origin", url))
+                await instant_answer.edit_text(
+                    hlink(result["link_text"], result["server_url"])
+                    + "\n"
+                    + hlink("#origin", url)
+                )
             await message.delete()
         else:
             await message.answer("Something went wrong...")
-      
-    except Exception as e:
+
+    except Exception:
         logging.error(traceback.format_exc())
         await message.answer("Something went wrong...")
 
@@ -131,10 +159,16 @@ def check_url(url):
         "youtube": re.compile(r"^https://(?:www.)?(?:music.)?youtu(?:.be/|be.com/)?"),
         "soundcloud": re.compile(r"^https://(m|on)?.?soundcloud"),
         "yandex": re.compile(r"^https?://music\.yandex\.(?P<tld>ru|kz|ua|by|com)"),
-        "rutube": re.compile(r"^https?://rutube\.ru/(?:(?:live/)?video(?:/private)?|(?:play/)?embed)/(?P<id>[\da-z]{32})"),
-        "coub" :  re.compile(r"^(?:coub:|https?://(?:coub\.com/(?:view|embed|coubs)/|c-cdn\.coub\.com/fb-player\.swf\?.*\bcoub(?:ID|id)=))(?P<id>[\da-z]+)"),
+        "rutube": re.compile(
+            r"^https?://rutube\.ru/(?:(?:live/)?video(?:/private)?|(?:play/)?embed)/(?P<id>[\da-z]{32})"
+        ),
+        "coub": re.compile(
+            r"^(?:coub:|https?://(?:coub\.com/(?:view|embed|coubs)/|c-cdn\.coub\.com/fb-player\.swf\?.*\bcoub(?:ID|id)=))(?P<id>[\da-z]+)"
+        ),
         "tiktok": re.compile(r"^https://(?:www.)?(?:vt.)?tiktok.com/"),
-        "instagram": re.compile(r"(?P<url>https?://(?:www\.)?instagram\.com(?:/(?!share/)[^/?#]+)?/(?:p|tv|reels?(?!/audio/))/(?P<id>[^/?#&]+))"),
+        "instagram": re.compile(
+            r"(?P<url>https?://(?:www\.)?instagram\.com(?:/(?!share/)[^/?#]+)?/(?:p|tv|reels?(?!/audio/))/(?P<id>[^/?#&]+))"
+        ),
     }
     for key in supported_urls:
         if supported_urls[key].search(url):
@@ -146,9 +180,9 @@ def create_download_dialog(key) -> dict:
     options = {
         "youtube": ("audio", "video"),
         "soundcloud": ("audio"),
-        "yandex": ("audio"), 
-        "rutube": ("audio","video"),
-        "coub" : ("audio", "video"),
+        "yandex": ("audio"),
+        "rutube": ("audio", "video"),
+        "coub": ("audio", "video"),
         "tiktok": ("video"),
         "instagram": ("video"),
     }
@@ -159,30 +193,42 @@ def create_download_dialog(key) -> dict:
     }
 
     kbd = InlineKeyboardBuilder()
-    text_and_data = [ buttons[btn] for btn in buttons if btn in options[key] ] + [("❌", "exit")]
-    row_btns = (InlineKeyboardButton(text = text, callback_data = data) for text, data in text_and_data)
+    text_and_data = [buttons[btn] for btn in buttons if btn in options[key]] + [
+        ("❌", "exit")
+    ]
+    row_btns = (
+        InlineKeyboardButton(text=text, callback_data=data)
+        for text, data in text_and_data
+    )
     kbd.row(*row_btns)
-    return {"text": "Может качнем эту ☝ ссылку?", "reply_markup": kbd.as_markup() }
+    return {"text": "Может качнем эту ☝ ссылку?", "reply_markup": kbd.as_markup()}
 
 
 dp = Dispatcher()
 
+
 @dp.callback_query(StateFilter(DownloadActions.select_action))
 async def inline_kb_answer_callback_handler(query: CallbackQuery, state: FSMContext):
     await query.answer()
-    await query.bot.delete_message(chat_id = query.message.chat.id, message_id = query.message.message_id)
+    if query.bot and query.message:
+        await query.bot.delete_message(
+            chat_id=query.message.chat.id, message_id=query.message.message_id
+        )
     if query.data != "exit":
         await process_message(USER_DATA[query.from_user.id], query.data == "video")
     await state.clear()
- 
+
+
 @dp.message()
 async def on_process_message(message: Message, state: FSMContext):
     if message and message.text:
         url_type = check_url(message.text)
         if url_type:
-            USER_DATA[message.from_user.id] = message
+            if message.from_user:
+                USER_DATA[message.from_user.id] = message
             await message.answer(**create_download_dialog(url_type))
             await state.set_state(DownloadActions.select_action)
+
 
 @dp.channel_post()
 async def on_process_channel_post(message: Message):
@@ -190,19 +236,31 @@ async def on_process_channel_post(message: Message):
         if check_url(message.text):
             await process_message(message, False)
 
+
 ##############################################################
 
-async def send_audio_message(bot : Bot, download):
+
+async def send_audio_message(bot: Bot, download):
     if download["file_size"] < TG_MAX_FILE_SIZE:
-        await bot.send_audio(download["user_id"],
-            FSInputFile(download["file_path"]), 
-            title = download["title"],
-            performer = download["performer"],
-            caption = hlink("#origin", download["url"]) + "  " + hlink("#file", download["server_url"]))
+        await bot.send_audio(
+            download["user_id"],
+            FSInputFile(download["file_path"]),
+            title=download["title"],
+            performer=download["performer"],
+            caption=hlink("#origin", download["url"])
+            + "  "
+            + hlink("#file", download["server_url"]),
+        )
     else:
-        await bot.send_message(download["user_id"], hlink(download["link_text"], download["server_url"]) + "\n" + hlink("#origin", download["url"]))
+        await bot.send_message(
+            download["user_id"],
+            hlink(download["link_text"], download["server_url"])
+            + "\n"
+            + hlink("#origin", download["url"]),
+        )
     download["delivered"] = int(True)
-    await DB.save_download(download)       
+    await DB.save_download(download)
+
 
 def scan_playlist(sub, playlist):
     urls = []
@@ -213,26 +271,32 @@ def scan_playlist(sub, playlist):
                     urls.append(ent["url"])
         else:
             urls.append(ent["url"])
-    urls.reverse() # elders first
+    urls.reverse()  # elders first
     return urls
 
 
-async def watchdog(bot : Bot, DB: Database):
+async def watchdog(bot: Bot, DB: Database):
     while True:
         await asyncio.sleep(60)
-        try:   
-            subscriptions = await DB.get_subscriptions()      
+        try:
+            subscriptions = await DB.get_subscriptions()
             for sub in subscriptions:
-                if sub["last_check"] == None or sub["last_check"] + timedelta(seconds = sub["interval"]) < datetime.now():
+                if (
+                    sub["last_check"] is None
+                    or sub["last_check"] + timedelta(seconds=sub["interval"])
+                    < datetime.now()
+                ):
                     sub["last_check"] = datetime.now()
                     await DB.save_subscription(sub)
-                    
+
                     playlist = await download_playlist(sub["url"], "1,2")
                     for url in scan_playlist(sub, playlist):
                         result = await DB.find_url(url, False, sub["user_id"])
                         if result:
                             if not result["delivered"]:
-                                logging.info(f"download {url} not delivered: trying resend")
+                                logging.info(
+                                    f"download {url} not delivered: trying resend"
+                                )
                                 enrich_info(result)
                                 result["url"] = url
                                 result["user_id"] = sub["user_id"]
@@ -242,20 +306,27 @@ async def watchdog(bot : Bot, DB: Database):
                             result = await download(url, False, sub["user_id"])
                             if result:
                                 await send_audio_message(bot, result)
-        except Exception as e:
+        except Exception:
             logging.error(traceback.format_exc())
 
 
 async def main():
     global DB
     DB = await Database.create()
-    bot = Bot(token = SETTINGS["telegram-api-token"], default = DefaultBotProperties(parse_mode = "HTML"))
+    bot = Bot(
+        token=SETTINGS["telegram-api-token"],
+        default=DefaultBotProperties(parse_mode="HTML"),
+    )
     _watchdog = asyncio.create_task(watchdog(bot, DB))
     _polling = dp.start_polling(bot)
     await asyncio.gather(_watchdog, _polling)
 
+
 if __name__ == "__main__":
-    logging.basicConfig(format="%(asctime)s %(levelname)-8s %(message)s", level = logging.INFO, datefmt="%Y-%m-%d %H:%M:%S")
-    dp.update.outer_middleware( SecurityMiddleware() )
+    logging.basicConfig(
+        format="%(asctime)s %(levelname)-8s %(message)s",
+        level=logging.INFO,
+        datefmt="%Y-%m-%d %H:%M:%S",
+    )
+    dp.update.outer_middleware(SecurityMiddleware())
     asyncio.run(main())
- 
--- database.py
+++ database.py
@@ -1,26 +1,44 @@
+import logging
+from datetime import datetime
 
 import aiosqlite
-from datetime import datetime
-import logging
 
 
 class Database:
     _instance = None
-    
-    downloads_attr = ["date", "user_id", "url", "video", "file_name", "file_size", "title", "artist", "channel", "delivered"]
+    _conn = aiosqlite.Connection
 
-    def __new__(self, *args, **kwargs):
-        if self._instance is None:
-            self._instance = super().__new__(self, *args, **kwargs)
-        return self._instance
-    
+    downloads_attr = [
+        "date",
+        "user_id",
+        "url",
+        "video",
+        "file_name",
+        "file_size",
+        "title",
+        "artist",
+        "channel",
+        "delivered",
+    ]
+
+    def __new__(cls, *args, **kwargs):
+        if cls._instance is None:
+            cls._instance = super().__new__(cls, *args, **kwargs)
+        return cls._instance
+
     @classmethod
     async def create(cls):
         # if not os.path.isfile("downloads.db"):
         conn = await aiosqlite.connect("downloads.db")
-        await conn.execute("CREATE TABLE IF NOT EXISTS downloads (date DATETIME, user_id STRING, url STRING, video BOOLEAN, file_name STRING, file_size BIGINT, title STRING)")
-        await conn.execute("CREATE UNIQUE INDEX IF NOT EXISTS `idx_downloads` ON `downloads` (`url`, `video`, `user_id`)")
-        await conn.execute("CREATE TABLE IF NOT EXISTS subscriptions (id INTEGER  NOT NULL PRIMARY KEY AUTOINCREMENT, user_id INTEGER, url STRING, keywords STRING, interval INTEGER, last_check DATETIME)")
+        await conn.execute(
+            "CREATE TABLE IF NOT EXISTS downloads (date DATETIME, user_id STRING, url STRING, video BOOLEAN, file_name STRING, file_size BIGINT, title STRING)"
+        )
+        await conn.execute(
+            "CREATE UNIQUE INDEX IF NOT EXISTS `idx_downloads` ON `downloads` (`url`, `video`, `user_id`)"
+        )
+        await conn.execute(
+            "CREATE TABLE IF NOT EXISTS subscriptions (id INTEGER  NOT NULL PRIMARY KEY AUTOINCREMENT, user_id INTEGER, url STRING, keywords STRING, interval INTEGER, last_check DATETIME)"
+        )
         await conn.commit()
         inst = cls()
         inst._conn = conn
@@ -29,57 +47,78 @@ class Database:
     async def save_download(self, download):
         row = {}
         for key in self.downloads_attr:
-            if not key in download:
+            if key not in download:
                 row[key] = None
-                logging.info(f"save_download reconciliate: key {key} not pesent in given dictionary, appended empty one")
+                logging.info(
+                    f"save_download reconciliate: key {key} not pesent in given dictionary, appended empty one"
+                )
             else:
                 row[key] = download[key]
-        await self._conn.execute("""INSERT INTO 
-            downloads(date, user_id, url, video, file_name, file_size, title, artist, channel, delivered) 
+        await self._conn.execute(
+            """INSERT INTO
+            downloads(date, user_id, url, video, file_name, file_size, title, artist, channel, delivered)
             VALUES(:date, :user_id, :url, :video, :file_name, :file_size, :title, :artist, :channel, :delivered)
             ON CONFLICT(url, video, user_id) DO UPDATE SET
             delivered = excluded.delivered""",
-            row
+            row,
         )
         await self._conn.commit()
 
-    async def find_url(self, url, video = False, user_id = None):
+    async def find_url(self, url, video=False, user_id=None) -> dict | None:
         if user_id:
-            cursor = await self._conn.execute("SELECT date, file_name, file_size, title, artist, channel, delivered FROM downloads WHERE url = ? and video = ? and user_id = ?", [url, int(video), user_id])
+            cursor = await self._conn.execute(
+                "SELECT date, file_name, file_size, title, artist, channel, delivered FROM downloads WHERE url = ? and video = ? and user_id = ?",
+                [url, int(video), user_id],
+            )
         else:
-            cursor = await self._conn.execute("SELECT date, file_name, file_size, title, artist, channel, delivered FROM downloads WHERE url = ? and video = ?", [url, int(video)])
+            cursor = await self._conn.execute(
+                "SELECT date, file_name, file_size, title, artist, channel, delivered FROM downloads WHERE url = ? and video = ?",
+                [url, int(video)],
+            )
         fetch = await cursor.fetchone()
-        return {
-            "date" : datetime.strptime(fetch[0][:10], "%Y-%m-%d").date(),
-            "file_name" : fetch[1],
-            "file_size" : fetch[2],
-            "title" : fetch[3],
-            "artist" : fetch[4],
-            "channel" : fetch[5],
-            "delivered" : fetch[6],
-        } if fetch else None
+        return (
+            {
+                "date": datetime.strptime(fetch[0][:10], "%Y-%m-%d").date(),
+                "file_name": fetch[1],
+                "file_size": fetch[2],
+                "title": fetch[3],
+                "artist": fetch[4],
+                "channel": fetch[5],
+                "delivered": fetch[6],
+            }
+            if fetch
+            else None
+        )
 
     async def get_subscriptions(self):
-        cursor = await self._conn.execute("SELECT id, user_id, url, keywords, interval, last_check FROM subscriptions")
+        cursor = await self._conn.execute(
+            "SELECT id, user_id, url, keywords, interval, last_check FROM subscriptions"
+        )
         fetch = await cursor.fetchall()
-        subs = [{
-            "id": ft[0],
-            "user_id" : ft[1],
-            "url": ft[2],
-            "keywords": ft[3],
-            "interval": ft[4],
-            "last_check": datetime.strptime(ft[5], "%Y-%m-%d %H:%M:%S.%f") if ft[5] else None
-        } for ft in fetch]
+        subs = [
+            {
+                "id": ft[0],
+                "user_id": ft[1],
+                "url": ft[2],
+                "keywords": ft[3],
+                "interval": ft[4],
+                "last_check": datetime.strptime(ft[5], "%Y-%m-%d %H:%M:%S.%f")
+                if ft[5]
+                else None,
+            }
+            for ft in fetch
+        ]
         return subs
 
     async def save_subscription(self, subscription):
-        await self._conn.execute("""INSERT INTO subscriptions(id, user_id, url, keywords, interval, last_check) 
+        await self._conn.execute(
+            """INSERT INTO subscriptions(id, user_id, url, keywords, interval, last_check)
             VALUES(:id, :user_id, :url, :keywords, :interval, :last_check)
             ON CONFLICT(id) DO UPDATE SET
                 keywords =  excluded.keywords,
                 interval =  excluded.interval,
                 last_check = excluded.last_check
             """,
-            subscription
+            subscription,
         )
         await self._conn.commit()
